8948201c11f1b5a5e3ad3140020dbc6b8769c248
Ilari Ranin <raninilari@gmail.com>
2025-06-10 15:09:33 +0300

    made backend

---
+++ osa_3/puhelinluettelo_backend/index.js
@@ -0,0 +1,86 @@
+const express = require("express")
+const cors = require('cors')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+let persons = [
+    { 
+      "name": "Arto Hellas", 
+      "number": "040-123456",
+      "id": "1"
+    },
+    { 
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523",
+      "id": "2"
+    },
+    { 
+      "name": "Dan Abramov", 
+      "number": "12-43-234345",
+      "id": "3"
+    },
+    { 
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122",
+      "id": "4"
+    }
+  ]
+
+app.get("/", (request, response) => {
+    response.send("<h1>Hello World!</h1>")
+})
+
+
+app.get("/api/persons", (request, response) => {
+    response.send(persons)
+})
+
+app.get("/api/persons/:id", (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    response.send(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post("/api/persons", (request, response) => {
+    if (!request.body.name || !request.body.number) {
+        return response.status(400).json({
+            error: "name and/or number missing"
+        })
+    }
+    if (persons.some((person) => person.name.toLowerCase() === request.body.name.toLowerCase())) {
+        return response.status(400).json({
+            error: "person already added"        
+    })
+}
+
+    const person = {
+        name: request.body.name,
+        number: request.body.number,
+        id: Math.floor(Math.random() * 10000)
+    }
+    persons = persons.concat(person)
+    
+    response.json(person)
+})
+
+app.get("/info", (request, response) => {
+    const date = new Date()
+    response.send(`
+            <p>Phonebook has info for ${persons.length} people</p>
+            ${date}
+            `)
+})
+
+const PORT = process.env.PORT || 3002
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
