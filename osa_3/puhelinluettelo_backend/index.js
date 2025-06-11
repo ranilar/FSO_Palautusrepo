abda62f0954e83db432cfdd182dd183402ab5813
Ilari Ranin <raninilari@gmail.com>
2025-06-11 16:06:44 +0300

    uibuild

--- osa_3/puhelinluettelo_backend/index.js
+++ osa_3/puhelinluettelo_backend/index.js
@@ -86,7 +86,7 @@ app.get("/info", (request, response) => {
             `)
 })
 
-const PORT = process.env.PORT || 3002
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
