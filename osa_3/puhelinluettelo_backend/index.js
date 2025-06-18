07a1ede705814c02cb0f2d044599549cc270eabf
Ilari Ranin <raninilari@gmail.com>
2025-06-18 13:06:20 +0300

    Tasks 3.15-3.18 done

--- osa_3/puhelinluettelo_backend/index.js
+++ osa_3/puhelinluettelo_backend/index.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
 const express = require("express")
-const cors = require('cors')
 const app = express()
 const Person = require("./models/person")
 
@@ -11,50 +10,49 @@ app.get("/", (request, response) => {
     response.send("<h1>Hello World!</h1>")
 })
 
-
 app.get("/api/persons", (request, response) => {
     Person
     .find({})
     .then(persons => {
       response.json(persons)
     })
+    .catch(error => next(error))
 })
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   const id = request.params.id
   Person.findById(id)
     .then(person => {
       if (person) {
         response.json(person)
+      } else {
+        response.status(404).end()
       }
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  console.log("persons before delete:", Person)
+app.delete('/api/persons/:id', (request, response, next) => {
   const id = request.params.id
   Person.findByIdAndDelete(id)
     .then(() => {
       response.status(204).end()
     })
-
-  console.log("persons after delete:", Person)
-  response.status(204).end()
+    .catch(error => next(error))
 })
 
-app.post("/api/persons", (request, response) => {
-    console.log("persons before post:", Person)
+app.post("/api/persons", (request, response, next) => {
     if (!request.body.name || !request.body.number) {
-        return response.status(400).json({
-            error: "name and/or number missing"
-        })
+        const error = new Error("Name and/or number missing")
+        error.name = "MissingInfo"
+        return next(error)
     }
     Person.findOne({ name: request.body.name })
     .then(existingPerson => {
       if (existingPerson) {
-        return response.status(400).json({
-          error: "person already added"
-        })
+        const error = new Error("Person already added")
+        error.name = "DuplicatePerson"
+        return next(error)
       }
     })
 
@@ -64,10 +62,26 @@ app.post("/api/persons", (request, response) => {
     })
     person
     .save()
-    .then(savedPerson => {
-    console.log("persons after post:", Person)
-    response.json(savedPerson)
-    })    
+      .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
+
+})
+
+app.put("/api/persons/:id", (request, response, next) => {
+    if (!request.body.number) {
+        const error = new Error("Number missing")
+        error.name = "MissingInfo"
+        return next(error)
+    }
+      Person.updateOne(
+        { name: { $regex: new RegExp(`^${request.body.name}$`, 'i') } },
+        { $set: { number: request.body.number } }
+  )      .then(savedPerson => {
+        response.json(savedPerson)
+      })
+      .catch(error => next(error))
 })
 
 app.get("/info", (request, response) => {
@@ -78,7 +92,28 @@ app.get("/info", (request, response) => {
             `)
 })
 
-const PORT = process.env.PORT || 3002
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === "MissingInfo") {
+    return response.status(400).json({ error: "name and/or number missing" })
+  }
+
+  if (error.name === "DuplicatePerson") {
+    return response.status(400).json({ error: "person already added" })
+  }
+
+  if (error.name === "CastError") {
+    return response.status(400).json({ error: "malformatted id" })
+  }
+
+  return response.status(500).json({ error: "internal server error" })
+}
+
+app.use(errorHandler)
+
