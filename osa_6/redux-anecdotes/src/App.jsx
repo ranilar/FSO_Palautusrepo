6c495a6cf8c6885a680e25bd95d0f1b583c54177
Ilari Ranin <raninilari@gmail.com>
2026-03-02 20:38:13 +0200

    6.3

--- osa_6/redux-anecdotes/src/App.jsx
+++ osa_6/redux-anecdotes/src/App.jsx
@@ -1,10 +1,14 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
 
   const vote = id => {
-    console.log('vote', id)
+    dispatch({
+      type: 'VOTE',
+      payload: id
+    })
   }
 
   return (
--- osa_6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa_6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -20,10 +20,19 @@ const asObject = anecdote => {
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+  switch (action.type) {
+    case 'VOTE':
+      const id = action.payload
 
-  return state
+      return state.map(anecdote =>
+        anecdote.id === id
+          ? { ...anecdote, votes: anecdote.votes + 1 }
+          : anecdote
+      )
+
+    default:
+      return state
+  }
 }
 
 export default reducer
