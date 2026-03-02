cfc57e49c4c458c59d20a606037faae286b1d018
Ilari Ranin <raninilari@gmail.com>
2026-03-02 20:27:31 +0200

    6.1-6.2 tasks

--- osa_6/unicafe-redux/src/reducers/counterReducer.js
+++ osa_6/unicafe-redux/src/reducers/counterReducer.js
@@ -1,3 +1,4 @@
+
 const initialState = {
   good: 0,
   ok: 0,
@@ -8,13 +9,22 @@ const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
-      return state
+      return {
+        ...state,
+        good: state.good + 1 
+      }
     case 'OK':
-      return state
+      return {
+        ...state,
+        ok: state.ok + 1 
+      }
     case 'BAD':
-      return state
+      return {
+        ...state,
+        bad: state.bad + 1 
+      }
     case 'RESET':
-      return state
+      return initialState
     default:
       return state
   }
