2be47dde6771d622dab0e7caeca4d0c26a91b51e
Ilari Ranin <raninilari@gmail.com>
2026-03-02 21:16:36 +0200

    6.8

--- osa_6/redux-anecdotes/src/App.jsx
+++ osa_6/redux-anecdotes/src/App.jsx
@@ -1,35 +1,12 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { voteAnecdote, createAnecdote } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const vote = id => {
-    dispatch(voteAnecdote(id))
-  }
-
-  const newAnecdote = (event) => {
-    event.preventDefault()
-
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
-  }
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      ))}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   )
