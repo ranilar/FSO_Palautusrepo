92f6674c33684213de6186830fa950139ee54632
Ilari Ranin <raninilari@gmail.com>
2026-03-03 18:39:28 +0200

    6.12

--- osa_6/redux-anecdotes/src/components/Notification.jsx
+++ osa_6/redux-anecdotes/src/components/Notification.jsx
@@ -1,4 +1,10 @@
+import { useSelector } from 'react-redux'
+
 const Notification = () => {
+  const message = useSelector(state => state.notification)
+
+  if (!message) return null
+
   const style = {
     border: 'solid',
     padding: 10,
@@ -6,7 +12,11 @@ const Notification = () => {
     marginBottom: 10
   }
 
-  return <div style={style}>render here notification...</div>
+  return (
+    <div style={style}>
+      {message}
+    </div>
+  )
 }
 
 export default Notification
