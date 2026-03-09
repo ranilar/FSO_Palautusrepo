a06f50550e57e9b3d601e4d31e35665de6952d1d
Ilari Ranin <raninilari@gmail.com>
2026-03-09 16:10:08 +0200

    7.4

--- osa_7/routed-anecdotes/src/App.jsx
+++ osa_7/routed-anecdotes/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Routes, Route, Link, useNavigate, useParams } from 'react-router-dom'
+import { useField } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -71,25 +72,25 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     
-    props.setNotification(`a new anecdote '${content}' created!`)
+    props.setNotification(`a new anecdote '${content.value}' created!`)
     
-    setContent('')
-    setAuthor('')
-    setInfo('')
+    content.reset()
+    author.reset()
+    info.reset()
     
     navigate('/')
   }
@@ -100,15 +101,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
--- osa_7/routed-anecdotes/vite.config.js
+++ osa_7/routed-anecdotes/vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     watch: {
-      ignored: ['**/node_modules/**', '**/.venv/**', '**/venv/**', '**/.git/**']
+      ignored: ['**/node_modules/**', '**/.git/**']
     }
   }
 })
