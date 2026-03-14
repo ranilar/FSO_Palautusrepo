426c8f436490014b7eeabbdbc001a70e064c1906
Ilari Ranin <raninilari@gmail.com>
2026-03-14 13:03:59 +0200

    small fixes

--- osa_7/bloglist/bloglist-frontend/src/services/login.js
+++ osa_7/bloglist/bloglist-frontend/src/services/login.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3003/api/login'
+const baseUrl = 'http://localhost:3001/api/login'
 
 const login = async credentials => {
+  console.log('login credentials', credentials)
   const response = await axios.post(baseUrl, credentials)
   console.log(response.data)
   return response.data
